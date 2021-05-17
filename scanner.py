74dc7a32fab55df4576b771618c2c2d21ceb53ad
David Schumm <46762601+davidschumm@users.noreply.github.com>
2021-05-16 18:34:18 -0700

    uploaded first test
    will probably need to rewrite its not working well on the last image

--- scanner.py
+++ scanner.py
@@ -1,41 +1,40 @@
 import cv2
-import os
 import numpy as np
-import mapper 
-
+import mapper
 image=cv2.imread("test_img.jpg")
 image=cv2.resize(image,(1300,800))
 orig=image.copy()
 
 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 cv2.imshow("Title",gray)
-cv2.waitKey(0)
 
 blurred=cv2.GaussianBlur(gray,(5,5),0)
-#first 2 is matrix muliplication. 3rd afrument is sigma. if not sure put 0
 cv2.imshow("Blur",blurred)
-cv2.waitKey(0)
 
 edged=cv2.Canny(blurred,30,50)
 cv2.imshow("Canny",edged)
-cv2.waitKey(0)
 
-contours,hierarchy = cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+contours,hierarchy=cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 contours=sorted(contours,key=cv2.contourArea,reverse=True)
 
 for c in contours:
-	p=cv2.arcLength(c,True)
-	approx=cv2.approxPolyDP(c,0.02*p,True)
+    p=cv2.arcLength(c,True)
+    approx=cv2.approxPolyDP(c,0.02*p,True)
 
-	if len(approx)==4:
-		target=approx
-		break
+    if len(approx)==4:
+        target=approx
+        break
 approx=mapper.mapp(target)
 
-pts = np.float32([[0,0],[800,0],[800,800],[0,800]])
-#faceCascade = cv2.CascadeClassifier( os.path.join(cv2.data.haarcascades, "haarcascade_frontalface_default.xml") )
+pts=np.float32([[0,0],[800,0],[800,800],[0,800]])
+
 op=cv2.getPerspectiveTransform(approx,pts)
 dst=cv2.warpPerspective(orig,op,(800,800))
 
+
 cv2.imshow("Scanned",dst)
+
+
+# press q or Esc to close
 cv2.waitKey(0)
+cv2.destroyAllWindows()
